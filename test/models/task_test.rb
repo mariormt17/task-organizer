bc55d36b94bab03c494198f705f17fa1cef840c1
Mario Mancilla <mariormt17@gmail.com>
2021-02-26 18:04:29 -0600

    Add schema information on models with annotate gem

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :bigint           not null, primary key
+#  name        :string
+#  description :text
+#  due_date    :date
+#  category_id :bigint           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
